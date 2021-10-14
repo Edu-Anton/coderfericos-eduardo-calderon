2b679fba0e5801118c3d2a39373c1601d9a572f0
Edu-Anton <eduardo1acc@gmail.com>
2021-10-13 23:04:55 -0500

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import logo from '../assets/logo.png';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-success bg-gradient text-light">
+      <div className="container-fluid">
+        <a className="text-light text-decoration-none" href="/">
+          <img className="img-fluid" src={logo} alt="logo" />
+          Coderféricos
+        </a>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link text-light" href="/">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-light" href="/">Categorías</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-light" href="/">Carrito</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
